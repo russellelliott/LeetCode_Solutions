8e8cca24c45c24acfd47f0c5152744087e7782e9
Russell Elliott <77514172+russellelliott@users.noreply.github.com>
2023-01-03 15:42:32 -0800

    Time: 6221 ms (5.04%), Space: 13.4 MB (76.74%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -1,8 +1,8 @@
 class Solution(object):
     def factorial(n):
-        for i in range(1, n+1):
-            fact = fact * i
-        return fact
+        if n==0 or n==1:
+            return 1
+        return n * factorial(n-1)
     def trailingZeroes(self, n):
         """
         :type n: int
