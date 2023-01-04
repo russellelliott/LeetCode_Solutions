7b0a0ecefc91ef7ff83a36375753676db54442a7
Russell Elliott <77514172+russellelliott@users.noreply.github.com>
2023-01-03 16:03:02 -0800

    Time: 85 ms (9.34%), Space: 14 MB (81.36%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        def binary(arr, target, low, high):
+            if low>high:
+                print(low, high)
+                #return -1
+                return low
+            
+            mid = (low+high)/2
+            if target == arr[mid]:
+                return mid
+            
+            elif target > arr[mid]:
+                #target on right
+                return binary(arr, target, mid+1, high)
+            
+            else:
+                #target on left
+                return binary(arr, target, low, mid-1)
+        
+        return binary(nums, target, 0, len(nums)-1)
+
+#reused: https://leetcode.com/problems/binary-search/submissions/
