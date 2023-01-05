c17e2faefc032f6aaed224fddef7afda2c108ac2
Russell Elliott <77514172+russellelliott@users.noreply.github.com>
2023-01-05 13:01:42 -0800

    Time: 51 ms (7.65%), Space: 13.1 MB (99.41%) - LeetHub

---
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def checkAlmostEquivalent(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: bool
+        """
+        def makeDict(word):
+            #split word into chars
+            letters = word.split()
+            #initalize empty dictionary
+            freq = {}
+
+            #iterate through the list of numbers
+            for i in word:
+                if i in freq:
+                    freq[i] = freq[i]+1
+                else:
+                    freq[i] = 1
+            
+            return freq
+
+        #make a dictionary for each of the characters of word1 and word2
+        #compare frequencies of each
+        dict1 = makeDict(word1) #frequencies in word1
+        dict2 = makeDict(word2) #frequencies in word2
+        allLetters = makeDict(word1+word2) #all letters across all words
+        
+        def getFrequency(dictionary, word):
+            #print(word)
+            if word in dictionary: #if it exits
+                return dictionary[word]
+            return 0
+        for letter in allLetters:
+            freq1 = getFrequency(dict1, letter)
+            freq2 = getFrequency(dict2, letter)
+            #print(letter, freq1, freq2)
+            if abs(freq1-freq2) >3:
+                return False
+        
+        #false never invoked, return true
+        return True
