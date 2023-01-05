30aeeb3b68a66abc34c92f3502827632b3be0dc2
Russell Elliott <77514172+russellelliott@users.noreply.github.com>
2023-01-05 10:45:54 -0800

    Time: 4644 ms (46.70%), Space: 98.9 MB (13.22%) - LeetHub

---
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteMiddle(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        node = head
+        length = 0
+        while node:
+            node = node.next
+            length = length+1
+        
+        print("length: ", length)
+        if length==1:
+            return head.next
+        
+        node = head
+        #go floor(length/2) times forward in list)
+        for i in range(length//2-1):
+            node = node.next
+        
+        #perform the delete
+        node.next = node.next.next
+        
+        return head
+
+#delete middle node
+#first, find length
+#then go to length/2 node
+
+#if length is 2, middle is 2 (use floor?)
+
+#reused: https://leetcode.com/problems/remove-nth-node-from-end-of-list/submissions/
